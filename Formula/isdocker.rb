b72497c1bfe5ed21677ba3a2eab5980c355c94f9
hng.atuan@gmail.com <tuanha@bsscommerce.com>
2023-09-20 10:46:27 +0700

    main - modify formula

--- Formula/isdocker.rb
+++ Formula/isdocker.rb
@@ -2,17 +2,17 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Isdocker < Formula
-  desc "a CLI utility for orchestrating Docker based developer environments"
-  homepage "https://github.com/defenseoftheancients/isdocker"
+  desc ""
+  homepage ""
   url "https://github.com/defenseoftheancients/isdocker/archive/refs/tags/1.0.0.tar.gz"
   sha256 "023b45d397aa6a395ad6fa0131236ac9cc99f6ec5f58aa7655d1e0bbbe89e35e"
-  license "MIT"
+  license ""
 
   # depends_on "cmake" => :build
 
   def install
-    bin.install "defenseoftheancients/isdocker"
-    bin.install "bin/isdocker" => "isdocker"
+    bin.install "isdocker"
+    #bin.install "isdocker" => "isdocker"
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
