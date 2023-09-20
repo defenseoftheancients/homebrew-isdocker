f31316c22dd010e86e198380da87ee1a6da3213b
hng.atuan@gmail.com <tuanha@bsscommerce.com>
2023-09-20 10:40:26 +0700

    main - modify formula

--- Formula/isdocker.rb
+++ Formula/isdocker.rb
@@ -11,7 +11,7 @@ class Isdocker < Formula
   # depends_on "cmake" => :build
 
   def install
-    bin.install "isdocker"
+    bin.install "defenseoftheancients/isdocker"
     bin.install "bin/isdocker" => "isdocker"
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
